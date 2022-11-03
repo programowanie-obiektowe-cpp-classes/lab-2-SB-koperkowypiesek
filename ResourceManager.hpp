cea538b068f9621f869dee2d9bdbcbb226acd558
Szymon Bielenin <symonbielenin@gamil.com>
2022-11-03 16:26:50 +0100

    1 i 2 zadanie

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -1,8 +1,34 @@
 #pragma once
 
 #include "Resource.hpp"
+#include "iostream"
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+private:
+    Resource* r;
+
+public:
+    ResourceManager() { r = new Resource; }
+    ResourceManager(const ResourceManager& resource_men)
+        : r{(Resource*)memcpy(new Resource, resource_men.r, sizeof(*resource_men.r))}
+    {
+        std::cout << "konstruktor kopiujajacy\n";
+    }
+
+    ~ResourceManager() { delete r; }
+
+    ResourceManager& operator=(const ResourceManager& reMen)
+    {
+        std::cout << "przypisanie\n";
+        if (&reMen == this) {
+            return *this;
+        }
+        delete r;
+        r = (Resource*)memcpy(new Resource, reMen.r, sizeof(*reMen.r));
+
+        return *this;
+    }
+
+    double get() { return r->get(); }
 };
